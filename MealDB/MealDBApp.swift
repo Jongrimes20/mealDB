ed503dbbec4e5a6a573086e55c1e9f58d073a767
Jongrimes20 <JonGrimes20@gmail.com>
2024-08-06 15:55:51 -0500

    Initial Commit

---
+++ MealDB/MealDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDBApp.swift
+//  MealDB
+//
+//  Created by Jon Grimes on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MealDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
